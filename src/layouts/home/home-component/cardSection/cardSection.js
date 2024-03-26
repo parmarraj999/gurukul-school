fe8ff5870aa85d26fec1bc14ccd7aca0c65c304b
parmarraj999 <rajraja5122@gmail.com>
2024-03-26 13:43:01 +0530

    home-card done

---
+++ src/layouts/home/home-component/cardSection/cardSection.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./cardSection.css"
+
+function CardSection() {
+    return (
+        <div className='card-section' >
+            <div className='card card1' >
+                <div className='card-text'>
+                    <h2>600+</h2>
+                    <h6>Students</h6>
+                </div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took </p>
+            </div>
+            <div className='card card2' >
+                <div className='card-text'>
+                    <h2>50+</h2>
+                    <h6>Teachers</h6>
+                </div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took </p>
+            </div>
+            <div className='card card3' >
+                <div className='card-text'>
+                    <h2>20+</h2>
+                    <h6>Years Experience</h6>
+                </div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took </p>
+            </div>
+        </div>
+    )
+}
+
+export default CardSection
--- src/layouts/home/home.jsx
+++ src/layouts/home/home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import "./home.css"
 import Header from './home-component/header/header'
 import AddmissionBtn from '../../components/addmission-btn/addmissionBtn'
+import CardSection from './home-component/cardSection/cardSection'
 
 function Home() {
   return (
     <div className='home-container'>
-      <Header/>
-      <AddmissionBtn/>
+      <Header />
+      <div className='home-wrapper' >
+        <CardSection />
+      </div>
+      <AddmissionBtn />
     </div>
   )
 }
